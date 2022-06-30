f5612b3dfdc9262c15bf06c93173cf0decfd8245
Tony_Chen <tony_chen@epam.com>
2022-06-30 15:00:21 +0800

    add second code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("add one code");
+		System.out.println("add second code");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
