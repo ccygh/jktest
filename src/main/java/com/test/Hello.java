fce9081c4bc631626a98d3d719004b13a412bcdf
Tony_Chen <tony_chen@epam.com>
2022-06-30 14:50:15 +0800

    add sprint boot codes

--- src/main/java/com/test/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello jenkins");
-        //add one code
-        //add second code
-    }
-}
